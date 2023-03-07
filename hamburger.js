3da8831b8dce8ae8df3ee32177ab3f7d2f5bacee
kayemba <kayembejonathan5@gmail.com>
2023-03-07 08:37:24 +0200

    fix linters

--- hamburger.js
+++ hamburger.js
@@ -1,13 +1,12 @@
-const hamburger = document.querySelector('.hamburger')
-const navnavMenu = document.querySelector('.navnav-menu')
+const hamburger = document.querySelector('.hamburger');
+const navnavMenu = document.querySelector('.navnav-menu');
 
 hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active')
-  navnavMenu.classList.toggle('active')
-})
+  hamburger.classList.toggle('active');
+  navnavMenu.classList.toggle('active');
+});
 
-document.querySelectorAll('navnav-link').forEach(n =>
-  n.addEventListener('click', () => {
-    hamburger.classList.remove('active')
-    hamburger.classList.remove('active')
-  }))
+document.querySelectorAll('navnav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  hamburger.classList.remove('active');
+}));
