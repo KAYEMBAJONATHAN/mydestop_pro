e84b75c771820d65da5f1a1ab4ed38c1c5c56b9d
kayemba <kayembejonathan5@gmail.com>
2023-03-07 16:10:01 +0200

    updated hamburger.js and the index.html

--- hamburger.js
+++ hamburger.js
@@ -1,16 +1,12 @@
 const hamburger = document.querySelector('.hamburger');
 const navnavMenu = document.querySelector('.navnav-menu');
-const contactLink = document.querySelector('.contact-link');
-const aboutLink = document.querySelector('.about-link');
-const portfolioLink = document.querySelector('.portfolio-link');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navnavMenu.classList.toggle('active');
 });
 
-
-navnavMenu.addEventListener('click', ()=> {
+navnavMenu.addEventListener('click', () => {
   hamburger.classList.remove('active');
   navnavMenu.classList.remove('active');
-})
+});
