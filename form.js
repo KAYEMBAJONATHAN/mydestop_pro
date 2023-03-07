6b8ff1cc08be293282aa185e777b9d791f7cd726
kayemba <kayembejonathan5@gmail.com>
2023-03-07 17:04:15 +0200

    add form contact

--- form.js
+++ form.js
@@ -2,15 +2,7 @@ const form = document.querySelector('.form');
 const submit = document.getElementById('get-in-touch-button');
 
 function checkUppercase(event) {
-  const email = document.getElementById('email').value;
-  const erroMessagediv = document.querySelector('.error-message-div');
 
-  if (erroMessagediv.querySelector('p') !== null) {
-    const paragraph = erroMessagediv.querySelector('p');
-    erroMessagediv.removeChild(paragraph);
-    erroMessagediv.getElementsByClassName.display = 'none';
-    submit.style.marginTop = '40px';
-  }
 
   if (email.toLowerCase() !== email) {
     const errorMessage = 'Please make sure that your emails do not contain any upper case caracters';
